b3dbde83cc8be26b24db80482dc8310ce8fe6e96
Augusto <lagrange.tesla@gmail.com>
2020-09-27 12:53:23 -0500

    Se reconstruyeron instancias de Todos

--- src/classes/todo.class.js
+++ src/classes/todo.class.js
@@ -1,5 +1,18 @@
 export class Todo {
-	constructor(tarea){
+
+	static fromJson({ id, tarea, completado, creado }) {
+
+		const tempTodo = new Todo(tarea);
+		tempTodo.id = id;
+		tempTodo.completado = completado;
+		tempTodo.creado = creado;
+
+		return tempTodo;
+	}
+
+
+
+	constructor(tarea) {
 
 		this.tarea = tarea;
 
@@ -7,4 +20,10 @@ export class Todo {
 		this.completado = false;
 		this.creado = new Date();
 	}
+
+	imprimirClase() {
+		console.log(`${this.tarea}-${this.id}`);
+	}
+
+
 }
--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ import { crearTodoHtml } from './js/componentes'
 export const todoList = new TodoList();
 
 todoList.todos.forEach( crearTodoHtml );
+
+console.log('todos', todoList.todos);
